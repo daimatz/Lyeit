13e239476ea28f762affdc977bfa105678b25119
daimatz <dai@daimatz.net>
2013-06-19 16:25:17 +0900

    refactor: qualified import for Text.Hastache and Text.Hastache.Context

--- src/Web/Lyeit/Html.hs
+++ src/Web/Lyeit/Html.hs
@@ -18,22 +18,21 @@ import           System.FilePath         (pathSeparators, splitDirectories,
                                           (</>))
 import           System.Locale           (defaultTimeLocale)
 import           System.Posix            (getFileStatus, modificationTime)
-
-import           Text.Hastache
-import           Text.Hastache.Context
+import qualified Text.Hastache           as H
+import qualified Text.Hastache.Context   as HC
 
 import           Web.Lyeit.Config
 import           Web.Lyeit.Type
 
-mustache :: MonadIO m => FilePath -> MuContext m -> m Text
+mustache :: MonadIO m => FilePath -> H.MuContext m -> m Text
 mustache path context = return . decodeUtf8
-    =<< hastacheFile defaultConfig path context
+    =<< H.hastacheFile H.defaultConfig path context
 
-nullContext :: MonadIO m => MuContext m
-nullContext = mkStrContext $ const $ MuVariable ("" :: Text)
+nullContext :: MonadIO m => H.MuContext m
+nullContext = HC.mkStrContext $ const $ H.MuVariable ("" :: Text)
 
-errorRef :: MuType m
-errorRef = MuVariable ("error" :: Text)
+errorRef :: H.MuType m
+errorRef = H.MuVariable ("error" :: Text)
 
 headHtml :: FilePath -> Text -> Text -> ConfigM Text
 headHtml path title query = do
@@ -44,39 +43,39 @@ headHtml path title query = do
                         splitDirectories path
                 in  first (root </>) (head splited) : tail splited
 
-        context "title" = MuVariable title
-        context "query" = MuVariable query
-        context "path"  = MuVariable $ pathSeparators <> path
-        context "paths" = MuList $ map (mkStrContext . listContext) paths
+        context "title" = H.MuVariable title
+        context "query" = H.MuVariable query
+        context "path"  = H.MuVariable $ pathSeparators <> path
+        context "paths" = H.MuList $ map (HC.mkStrContext . listContext) paths
         context _       = errorRef
 
-        listContext tuple "partial" = MuVariable $ fst tuple
-        listContext tuple "request" = MuVariable $ snd tuple
+        listContext tuple "partial" = H.MuVariable $ fst tuple
+        listContext tuple "request" = H.MuVariable $ snd tuple
         listContext _     _         = errorRef
 
-    liftIO $ mustache "view/head.mustache" (mkStrContext context)
+    liftIO $ mustache "view/head.mustache" (HC.mkStrContext context)
 
 dirHtml :: FilePath -> ListFiles -> ConfigM Text
 dirHtml request cts
-    = liftIO $ mustache "view/dir.mustache" (mkStrContext context)
+    = liftIO $ mustache "view/dir.mustache" (HC.mkStrContext context)
   where
     context "directory"
-        = MuList $ map (mkStrContext . directory) (cts ! Directory)
+        = H.MuList $ map (HC.mkStrContext . directory) (cts ! Directory)
     context "document"
-        = MuList $ map (mkStrContext . document) (cts ! Document)
+        = H.MuList $ map (HC.mkStrContext . document) (cts ! Document)
     context "other"
-        = MuList $ map (mkStrContext . other) (cts ! Other)
+        = H.MuList $ map (HC.mkStrContext . other) (cts ! Other)
     context _
         = errorRef
     -- FIXME: other properties than path, ex: title...
-    directory dir  "path"  = MuVariable $ requestPath dir
-    directory dir  "title" = MuVariable dir
+    directory dir  "path"  = H.MuVariable $ requestPath dir
+    directory dir  "title" = H.MuVariable dir
     directory _    _       = errorRef
-    document  doc  "path"  = MuVariable $ requestPath doc
-    document  doc  "title" = MuVariable doc
+    document  doc  "path"  = H.MuVariable $ requestPath doc
+    document  doc  "title" = H.MuVariable doc
     document  _    _       = errorRef
-    other     ot   "path"  = MuVariable $ requestPath ot
-    other     ot   "title" = MuVariable ot
+    other     ot   "path"  = H.MuVariable $ requestPath ot
+    other     ot   "title" = H.MuVariable ot
     other     _    _       = errorRef
     requestPath path
         = (if null request then "" else "/" <> request) <> "/" <> path
@@ -90,7 +89,7 @@ footHtmlWithPath watchpath = do
 
     let mtime = posixSecondsToUTCTime $ realToFrac $ modificationTime stat
         form = formatTime defaultTimeLocale "updated: %F %T" mtime
-        context "updated" = MuVariable form
+        context "updated" = H.MuVariable form
         context _         = errorRef
 
-    liftIO $ mustache "view/foot.mustache" (mkStrContext context)
+    liftIO $ mustache "view/foot.mustache" (HC.mkStrContext context)
